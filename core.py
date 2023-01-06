fddc4f6bee8fe45fe1f6d6febce95094e81d34c2
jardinesdelareina <jardinesdelareina@mail.ru>
2023-01-06 03:46:51 +0300

    классы не нужны

--- core.py
+++ core.py
@@ -3,78 +3,67 @@ import pandas as pd
 import requests, time
 from binance.exceptions import BinanceAPIException as bae
 
+def get_data(symbol):
+    # Получение данных
+    try:
+        df = pd.DataFrame(CLIENT.get_historical_klines(symbol, '1m', '1000m UTC'))
+    except bae:
+        print(bae)
+        time.sleep(30)
+        df = pd.DataFrame(CLIENT.get_historical_klines(symbol, '1m', '1000m UTC'))
+    df = df.iloc[:,:6]
+    df.columns = ['Time', 'Open', 'High', 'Low', 'Close', 'Volume']
+    df = df.set_index('Time')
+    df.index = pd.to_datetime(df.index, unit='ms')
+    df = df.astype(float)
+    df['FastSMA'] = df.Close.rolling(window=3).mean()
+    df['SlowSMA'] = df.Close.rolling(window=100).mean()
+    return df
 
-class Antrade:
+def send_message(message):
+    # Алерт в Telegram
+    return requests.get(
+        'https://api.telegram.org/bot{}/sendMessage'.format(TELETOKEN), 
+        params=dict(chat_id=CHAT_ID, text=message)
+    )
 
-    def __init__(self, symbol, interval, qnty):
-        self.symbol = symbol
-        self.interval = interval
-        self.qnty = qnty
+def main(symbol, qnty, open_position=False):
+    # Логика
+    while True:
+        data = get_data(symbol)
+        order_volume = round(qnty/data.Close.iloc[-1], 3)
 
-    def get_data(self):
-        # Получение данных
-        try:
-            df = pd.DataFrame(CLIENT.get_historical_klines(self.symbol, self.interval, '1000m UTC'))
-        except bae:
-            print(bae)
-            time.sleep(30)
-            df = pd.DataFrame(CLIENT.get_historical_klines(self.symbol, self.interval, '1000m UTC'))
-        df = df.iloc[:,:6]
-        df.columns = ['Time', 'Open', 'High', 'Low', 'Close', 'Volume']
-        df = df.set_index('Time')
-        df.index = pd.to_datetime(df.index, unit='ms')
-        df = df.astype(float)
-        df['FastSMA'] = df.Close.rolling(window=3).mean()
-        df['SlowSMA'] = df.Close.rolling(window=100).mean()
-        return df
+        if not open_position:
+            if data.SlowSMA.iloc[-1] < data.FastSMA.iloc[-1] \
+            and data.SlowSMA.iloc[-1] >= data.FastSMA.iloc[-2]:
+                order = CLIENT.create_order(symbol=symbol, side='BUY', type='MARKET', quantity=order_volume)
+                buyprice = float(order['fills'][0]['price'])
+                message = symbol + ' Buy ' + str(buyprice)
+                send_message(message)
+                print(message)
+                open_position = True
+            else:
+                print(
+                    'Ожидание', 
+                    symbol, 
+                    round(data.SlowSMA.iloc[-1], 5), 
+                    round(data.FastSMA.iloc[-1], 5)
+                )
 
-    def send_message(self, message):
-        # Алерт в Telegram
-        return requests.get(
-            'https://api.telegram.org/bot{}/sendMessage'.format(TELETOKEN), 
-            params=dict(chat_id=CHAT_ID, text=message)
-        )
+        if open_position:
+            if data.SlowSMA.iloc[-1] > data.FastSMA.iloc[-1] \
+            and data.SlowSMA.iloc[-1] <= data.FastSMA.iloc[-2]:
+                order = CLIENT.create_order(symbol=symbol, side='SELL', type='MARKET', quantity=order_volume)
+                sellprice = float(order['fills'][0]['price'])
+                profit = round((sellprice - buyprice) * order_volume)
+                result = f'Результат = {profit}'      
+                message = symbol + ' Sell ' + str(result)
+                send_message(message)
+                print(message)
+                open_position = False
+            else:
+                print(f'Открыта позиция {symbol}')
 
-    def main(self, open_position=False):
-        # Логика
+        time.sleep(60)
 
-        while True:
-            data = self.get_data()
-            order_volume = round(self.qnty/data.Close.iloc[-1], 3)
-
-            if not open_position:
-                if data.SlowSMA.iloc[-1] < data.FastSMA.iloc[-1] \
-                and data.SlowSMA.iloc[-1] >= data.FastSMA.iloc[-2]:
-                    order = CLIENT.create_order(symbol=self.symbol, side='BUY', type='MARKET', quantity=order_volume)
-                    buyprice = float(order['fills'][0]['price'])
-                    message = self.symbol + ' Buy ' + str(buyprice)
-                    self.send_message(message)
-                    print(message)
-                    open_position = True
-                else:
-                    print(
-                        'Ожидание', 
-                        self.symbol, 
-                        round(data.SlowSMA.iloc[-1], 5), 
-                        round(data.FastSMA.iloc[-1], 5),
-                    )
-
-            if open_position:
-                if data.SlowSMA.iloc[-1] > data.FastSMA.iloc[-1] \
-                and data.SlowSMA.iloc[-1] <= data.FastSMA.iloc[-2]:
-                    order = CLIENT.create_order(symbol=self.symbol, side='SELL', type='MARKET', quantity=order_volume)
-                    sellprice = float(order['fills'][0]['price'])
-                    profit = round((sellprice - buyprice) * order_volume)
-                    result = f'Результат = {profit}'      
-                    message = self.symbol + ' Sell ' + str(result)
-                    self.send_message(message)
-                    print(message)
-                    open_position = False
-                else:
-                    print(f'Открыта позиция {self.symbol}')
-
-            time.sleep(60)
-
-
-bot= Antrade('XRPUSDT', '1m', 50)
-bot.main()
+main('BTCUSDT', 50)
