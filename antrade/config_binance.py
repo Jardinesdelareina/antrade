661cd3d03e18dfbf669643c6adc764363c6d1d4f
jardinesdelareina <jardinesdelareina@mail.ru>
2023-05-12 14:37:40 +0300

    фьючерсы в core

--- antrade/config_binance.py
+++ antrade/config_binance.py
@@ -4,5 +4,9 @@ import environs
 env = environs.Env()
 env.read_env('.env')
 
-# Binance API Spot
+# Binance API
 CLIENT = Client(env('API_KEY'), env('SECRET_KEY'), {'verify': True, 'timeout': 20})
+
+# Futures balance
+GENERAL_BALANCE = round(float(CLIENT.futures_account_balance()[8]['balance']), 2)
+AVAILABLE_BALANCE = round(float(CLIENT.futures_account_balance()[8]['withdrawAvailable']), 2)
--- antrade/core.py
+++ antrade/core.py
@@ -1,18 +1,18 @@
 import pandas as pd
-import requests, json
+import json
 from binance.helpers import round_step_size
 from antrade.utils import round_float
 from antrade.config_binance import CLIENT
 from antrade.utils import send_message
-from telegram.config_telegram import CHAT_ID, TELETOKEN
 
 
 class BinanceAPI:
     """ Базовый класс, источник передачи данных через API Binance
     """
 
-    def __init__(self, symbol, interval, qnty=50):
+    def __init__(self, market, symbol, interval, qnty=50):
         """ Конструктор класса BinanceAPI
+            market (str): Тип рынка
             symbol (str): Наименование тикера
             interval (str): Временной интервал
             qnty (float): Размер ордера
@@ -20,6 +20,7 @@ class BinanceAPI:
                                 если нет открытой позиции, значение атрибута - False,
                                 если ордер открыт - True
         """
+        self.market = market
         self.symbol = symbol
         self.interval = interval
         self.qnty = qnty
@@ -29,13 +30,24 @@ class BinanceAPI:
     def get_data(self):
         """ Получение данных 
         """
-        df = pd.DataFrame(CLIENT.get_historical_klines(symbol=self.symbol, interval=self.interval, limit=70))
+        match self.market:
+            case 'spot':
+                df = pd.DataFrame(CLIENT.get_historical_klines(
+                    symbol=self.symbol, 
+                    interval=self.interval, 
+                    limit=70
+                ))
+            case 'futures':
+                df = pd.DataFrame(CLIENT.futures_historical_klines(
+                    symbol=self.symbol, 
+                    interval=self.interval,
+                    start_str='1000m UTC'
+                ))
         df = df.iloc[:,:5]
         df.columns = ['Time', 'Open', 'High', 'Low', 'Close']
         df = df.set_index('Time')
         df.index = pd.to_datetime(df.index, unit='ms')
-        df = df.astype(float)
-        return df
+        return df.astype(float)
     
 
     def get_last_price(self) -> float:
@@ -45,20 +57,23 @@ class BinanceAPI:
         return df.Close.iloc[-1]
 
 
+class Spot(BinanceAPI):
+    """ Ордера рынка Spot
+    """
+
     def calculate_quantity(self) -> float:
         """ Расчет объема ордера 
         """
         symbol_info = CLIENT.get_symbol_info(self.symbol)
         step_size = symbol_info.get('filters')[1]['stepSize']
         order_volume = self.qnty / self.get_last_price()
-        order_volume = round_step_size(order_volume, step_size)
-        return order_volume
+        return round_step_size(order_volume, step_size)
 
 
     def place_order(self, order_side: str):
         """ Размещение ордеров
 
-            order_side (str): Направление ордера, передаваемое при вызове функции в алгоритме.
+            order_side (str): Направление ордера, передаваемое при вызове функции в алгоритме
         """
 
         if order_side == 'BUY':
@@ -95,3 +110,35 @@ class BinanceAPI:
             send_message(message)
             print(message)
             print(json.dumps(order, indent=4, sort_keys=True))
+
+
+class Futures(BinanceAPI):
+    """ Ордера рынка Futures
+    """
+
+    def calculate_quantity(self) -> float:
+        """ Расчет объема ордера 
+        """
+        INFO = CLIENT.futures_exchange_info()
+        for symbol in INFO['symbols']:
+            if symbol['symbol'] == self.symbol:
+                step_size = symbol['filters'][2]['stepSize']
+                return round_step_size((self.qnty / self.get_last_price()), step_size)
+            
+    
+    def place_order(self, order_side: str):
+        """ Размещение MARKET ордеров
+
+            order_side (str): Направление ордера, передаваемое при вызове функции в алгоритме
+        """
+        
+        order = CLIENT.futures_create_order(
+                symbol=self.symbol, 
+                side=order_side, 
+                type='MARKET', 
+                quantity=self.calculate_quantity(),
+        )
+        print(f'{self.symbol} {order_side} \n {json.dumps(order, indent=4, sort_keys=True)}')
+        message = f'{self.symbol} \n {order_side}: {self.get_last_price()}'
+        self.send_message(message)
+        
